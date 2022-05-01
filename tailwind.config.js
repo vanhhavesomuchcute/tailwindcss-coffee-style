cfcc435ff45f664e9fbe23e18a5fe26346f63320
Nguyễn Đình Việt Anh <vanhhavesomuchcute@gmail.com>
2022-05-01 15:54:36 +0700

    Responsive header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Karla: ['Karla', 'sans-serif']
+      },
       colors: {
         'light-coffee': '#C89F94'
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { opacity: 0, transform: 'translateY(-100%)' },
+                  
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown .8s ease-in-out forwards',
       }
     },
   },
