da3015ee1c0482d082201ba15bbaf5656c1aac8d
Nguyễn Đình Việt Anh <vanhhavesomuchcute@gmail.com>
2022-05-02 15:40:17 +0700

    Add_featured_mugs_and_product_card

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,12 @@ module.exports = {
         }
       },
       keyframes: {
-        slideDown: {
-          '0%': { opacity: 0, transform: 'translateY(-100%)' },
-                  
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        }
+        slideDown: { '0%': { opacity: 0, transform: 'translateY(-100%)' }, '100%': { opacity: 1, transform: 'translateY(0)' } },
+        fadeIn: { from: { opacity:0 }, to: { opacity:1 } }
       },
       animation: {
         slideDown: 'slideDown .8s ease-in-out forwards',
+        fadeIn: 'fadeIn .5s ease-in-out'
       }
     },
   },
